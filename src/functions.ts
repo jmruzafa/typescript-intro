209dbff87fd5c082288e3cdde5300a5213110760
Javier Ruzaafa <javier.reyes@adidas.com>
2021-11-22 17:52:01 +0100

    Functions

--- src/functions.ts
+++ src/functions.ts
@@ -3,3 +3,21 @@ function printName(f, l){
     console.log(greeting + f + " " + l);
 }
 printName("Javier", "Reyes");
+
+function gradeGenerator(grade: number): string {
+    if(grade < 60)
+        return 'F';
+    if(grade >= 60 && grade < 70)
+        return 'D';
+    if(grade >= 70 && grade < 80)
+        return 'C';
+    if(grade >= 80 && grade < 90)
+        return 'B';
+    return 'A'
+}
+
+console.log(gradeGenerator(45));
+console.log(gradeGenerator(60));
+console.log(gradeGenerator(75));
+console.log(gradeGenerator(86));
+console.log(gradeGenerator(95));
